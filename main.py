fe71e970e7f38f18c3cf255086d59dfbbe9700c7
ranjith0826 <ranjithka194@gmail.com>
2026-04-06 12:36:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 @app.route('/')
 def home():
   return render_template('index.html',title="home page")
-  if __name__ == '__main__' :
+if __name__ == '__main__' :
     app.run(host = '127.0.0.1',port=8000,debug = True)
